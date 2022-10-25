df0a99f1ab0daa591b647c56de4d80f9dfef8b30
despinosaa <88846665+despinosaa@users.noreply.github.com>
2022-10-24 23:08:56 -0500

    Add files via upload

---
+++ tallerTSC/data.ts
@@ -0,0 +1,20 @@
+import { Serie } from './serie.js';
+
+export const series = [
+    new Serie (1,"Better Call Saul","Netflix", 6,"Better Call Saul follows the transformation of Jimmy McGill (Bob Odenkirk), a former con artist who is trying to become a respectable lawyer, into the personality of the flamboyant criminal lawyer Saul Goodman, over the six-year period prior to the events of Breaking Bad, spanning from approximately 2002 to 2008." ,
+    "https://en.wikipedia.org/wiki/Better_Call_Saul","https://upload.wikimedia.org/wikipedia/en/2/2f/Better_Call_Saul_season_3.jpg") ,
+    
+    new Serie (2,"Grey's Anatomy", "ABC", 19, "Grey's Anatomy is an American medical drama television series that premiered on March 27, 2005, on ABC as a mid-season replacement. The series focuses on the lives of surgical interns, residents, and attendings as they develop into seasoned doctors while balancing personal and professional relationships. ", 
+    "https://abc.com/shows/greys-anatomy","https://m.media-amazon.com/images/M/MV5BODA2Mjk0N2MtNGY0Mi00ZWFjLTkxODEtZDFjNDg4ZDliMGVmXkEyXkFqcGdeQXVyMzAzNTY3MDM@._V1_.jpg"),
+    
+    new Serie (3, "Quantico", "Netflix", 3, "La serie sigue a unos jóvenes reclutas del FBI en su época de entrenamiento, valiéndose de flashbacks y flashforwards para descubrir quién de ellos es responsable del mayor ataque terrorista en Estados Unidos desde el ocurrido el 11 de septiembre de 2001.", "https://es.wikipedia.org/wiki/Quantico_(serie_de_televisi%C3%B3n)",
+      "https://pics.filmaffinity.com/Quantico_Serie_de_TV-212551692-large.jpg"),
+
+    new Serie (4, "Peaky Blinders", "Netflix", 6, "Peaky Blinders es una serie de televisión inglesa de drama histórico, emitida por el canal BBC Two. La serie está protagonizada por Cillian Murphy y se centra en una familia de gánsteres de Birmingham, durante los años veinte y del ascenso de su jefe, Thomas Shelby, un mafioso que dominará toda Inglaterra, después de afrontar una terrible guerra.",
+        "https://es.wikipedia.org/wiki/Peaky_Blinders", "https://m.media-amazon.com/images/M/MV5BZjYzZDgzMmYtYjY5Zi00YTk1LThhMDYtNjFlNzM4MTZhYzgyXkEyXkFqcGdeQXVyMTE5NDQ1MzQ3._V1_.jpg"),
+    
+    new Serie (5, "Gossip Girl", "Netflix",  4, "The show centers on the scandalous lives of attractive, privileged and very affluent teens who reside in Manhattan's Upper East Side (UES) and how they betray one another while being constantly observed by a vicious but well-known blogger.",
+        "https://en.wikipedia.org/wiki/Gossip_Girl", "https://es.web.img3.acsta.net/pictures/13/12/04/16/18/394160.jpg"),
+
+];
+   
